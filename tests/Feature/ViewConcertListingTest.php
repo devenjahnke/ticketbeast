32de4decb09e22a968556ec114246c344039a028
devenjahnke <contact@devenjahnke.com>
2021-02-03 14:14:04 -0500

    Sketching out Our First Test

---
+++ tests/Feature/ViewConcertListingTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature;
+
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Tests\TestCase;
+
+class ViewConcertListingTest extends TestCase
+{
+    /** @test */
+    function user_can_view_a_concert_listing()
+    {
+        // Arrange
+        // Create a concert
+        $concert = Concert::create([
+            'title' => 'The Red Chord',
+            'subtitle' => 'with Animosity and Lethargy',
+            'date' => Carbon::parse('December 13th, 2021 8:00pm'),
+            'ticket_price' => 3250,  // Stored as cents
+            'venue' => 'The Mosh Pit',
+            'venue_address' => '123 Example Lane',
+            'city' => 'Laraville',
+            'state' => 'ON',
+            'zip' => '17916',
+            'additional_information' => 'For tickets, call (555) 555-5555.',
+        ]);
+        // Act
+        // View the concert listing
+        $response = $this->get('/concerts/' . $concert->id);
+
+        // Assert
+        // See the concert details
+        $response->assertSee('The Red Chord');
+        $response->assertSee('with Animosity and Lethargy');
+        $response->assertSee('December 13th, 2021');
+        $response->assertSee('8:00pm');
+        $response->assertSee('32.50');
+        $response->assertSee('The Mosh Pit');
+        $response->assertSee('123 Example Lane');
+        $response->assertSee('Laraville, ON 17916');
+        $response->assertSee('For tickets, call (555) 555-5555.');
+    }
+
+}
