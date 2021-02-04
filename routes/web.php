e38da6718383d6b7dc22fbbab881b462af029c90
devenjahnke <contact@devenjahnke.com>
2021-02-04 02:03:29 -0500

    Getting to Green

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/concerts/{id}', [App\Http\Controllers\ConcertsController::class, 'show']);
--- tests/Feature/ViewConcertListingTest.php
+++ tests/Feature/ViewConcertListingTest.php
@@ -2,22 +2,29 @@
 
 namespace Tests\Feature;
 
+use App\Models\Concert;
 use Carbon\Carbon;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Tests\TestCase;
 
 class ViewConcertListingTest extends TestCase
 {
+    use DatabaseMigrations; // Run migrations when scaffolding sqlite database in memory
+
     /** @test */
     function user_can_view_a_concert_listing()
     {
+        // Disable Laravel's default exception handling
+        $this->withoutExceptionHandling();
+
         // Arrange
         // Create a concert
         $concert = Concert::create([
             'title' => 'The Red Chord',
             'subtitle' => 'with Animosity and Lethargy',
-            'date' => Carbon::parse('December 13th, 2021 8:00pm'),
+            'date' => Carbon::parse('December 13, 2021 8:00pm'),
             'ticket_price' => 3250,  // Stored as cents
             'venue' => 'The Mosh Pit',
             'venue_address' => '123 Example Lane',
@@ -26,6 +33,7 @@ class ViewConcertListingTest extends TestCase
             'zip' => '17916',
             'additional_information' => 'For tickets, call (555) 555-5555.',
         ]);
+
         // Act
         // View the concert listing
         $response = $this->get('/concerts/' . $concert->id);
@@ -34,7 +42,7 @@ class ViewConcertListingTest extends TestCase
         // See the concert details
         $response->assertSee('The Red Chord');
         $response->assertSee('with Animosity and Lethargy');
-        $response->assertSee('December 13th, 2021');
+        $response->assertSee('December 13, 2021');
         $response->assertSee('8:00pm');
         $response->assertSee('32.50');
         $response->assertSee('The Mosh Pit');
