920591f770fb391a89b2b3c95da83441bdc4bcc1
devenjahnke <contact@devenjahnke.com>
2021-02-15 02:28:45 -0500

    Decoupling Data from Presentation

--- tests/Feature/ViewOrderTest.php
+++ tests/Feature/ViewOrderTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Models\Concert;
 use App\Models\Order;
 use App\Models\Ticket;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 
@@ -17,11 +18,22 @@ class ViewOrderTest extends TestCase
     {
         $this->withoutExceptionHandling();
 
-        $concert = Concert::factory()->create();
+        $concert = Concert::factory()->create([
+            'title' => 'The Red Chord',
+            'subtitle' => 'with Animosity and Lethargy',
+            'date' => Carbon::parse('March 12, 2017 8:00pm'),
+            'ticket_price' => 4250,
+            'venue' => 'The Mosh Pit',
+            'venue_address' => '123 Example Lane',
+            'city' => 'Laraville',
+            'state' => 'ON',
+            'zip' => '17916',
+        ]);
         $order = Order::factory()->create([
             'confirmation_number' => 'ORDERCONFIRMATION1234',
             'card_last_four' => '1881',
             'amount' => 8500,
+            'email' => 'john@example.com'
         ]);
         $ticketA = Ticket::factory()->create([
             'concert_id' => $concert->id,
@@ -45,5 +57,13 @@ class ViewOrderTest extends TestCase
         $response->assertSee('**** **** **** 1881');
         $response->assertSee('TICKETCODE123');
         $response->assertSee('TICKETCODE456');
+        $response->assertSee('The Red Chord');
+        $response->assertSee('with Animosity and Lethargy');
+        $response->assertSee('The Mosh Pit');
+        $response->assertSee('123 Example Lane');
+        $response->assertSee('Laraville, ON');
+        $response->assertSee('17916');
+        $response->assertSee('john@example.com');
+        $response->assertSee('2017-03-12 20:00');   // time tag used to decouple data from presentation
     }
 }
