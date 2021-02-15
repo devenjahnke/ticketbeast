f4c68136b8f3e1848b3447d0d9a711820b2352aa
devenjahnke <contact@devenjahnke.com>
2021-02-15 03:17:06 -0500

    Testing the Confirmation Number Format

---
+++ tests/Unit/RandomOrderConfirmationNumberGeneratorTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\RandomOrderConfirmationNumberGenerator;
+use Tests\TestCase;
+
+class RandomOrderConfirmationNumberGeneratorTest extends TestCase
+{
+    /** @test */
+    function must_be_24_characters_long()
+    {
+        $generator = new RandomOrderConfirmationNumberGenerator;
+
+        $confirmationNumber = $generator->generate();
+
+        $this->assertEquals(24, strlen($confirmationNumber));
+    }
+
+    /** @test */
+    function can_only_contain_uppercase_letters_and_numbers()
+    {
+        $generator = new RandomOrderConfirmationNumberGenerator;
+
+        $confirmationNumber = $generator->generate();
+
+        $this->assertMatchesRegularExpression('/^[A-Z0-9]+$/', $confirmationNumber);
+    }
+
+    /** @test */
+    function cannot_contain_ambiguous_characters()
+    {
+        $generator = new RandomOrderConfirmationNumberGenerator;
+
+        $confirmationNumber = $generator->generate();
+
+        $this->assertFalse(strpos($confirmationNumber, '1'));
+        $this->assertFalse(strpos($confirmationNumber, 'I'));
+        $this->assertFalse(strpos($confirmationNumber, '0'));
+        $this->assertFalse(strpos($confirmationNumber, 'O'));
+    }
+
+}
