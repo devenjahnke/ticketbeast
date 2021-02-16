83894c6386ea08873c2500274cabb399b7bba8f3
devenjahnke <contact@devenjahnke.com>
2021-02-16 03:37:43 -0500

    Ready to Demo

--- public/js/app.js
+++ public/js/app.js
@@ -1937,6 +1937,7 @@ __webpack_require__.r(__webpack_exports__);
         ticket_quantity: this.quantity,
         payment_token: token.id
       }).then(function (response) {
+        window.location = "/orders/".concat(response.data.confirmation_number);
         console.log("Charge succeeded");
       })["catch"](function (response) {
         _this.processing = false;
